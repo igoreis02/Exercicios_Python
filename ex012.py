3f6f2e9315b85b93118af1aa815cc1377487c2b0
AlbertSouza <99365890+igoreis02@users.noreply.github.com>
2023-10-05 22:53:15 -0300

    Create ex012.py

---
+++ ex012.py
@@ -0,0 +1,3 @@
+produto = float(input('Qual o valor do produto?'))
+desconto = (produto*5)/100
+print('O produto que custava R${}, na promoção com desconto de 5% vai custar R${}' .format(produto, produto-desconto))
