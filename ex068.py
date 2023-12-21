f9095789ce12d8286d8ebdf896162848dd7e3b95
AlbertSouza <99365890+igoreis02@users.noreply.github.com>
2023-12-21 19:15:29 -0300

    Create ex068.py
    jogo par ou impar

---
+++ ex068.py
@@ -0,0 +1,23 @@
+from random import randint
+soma = 0
+qtd = 0
+print('Vamos jogar PAR OU IMPAR')
+print('-'*20)
+
+while True:
+    jogador = int(input('Diga um valor: '))
+    cond = str(input('PAR OU IMPAR [P / I]')).upper().strip()[0]
+
+    computador = randint(1, 10)
+    soma = computador + jogador
+    if soma % 2 == 0:
+        print(f'Você jogou {jogador} e o computador {computador}, Total {soma} deu Par')
+    if soma % 2 == 1:
+        print(f'Você jogou {jogador} e o computador {computador}, Total {soma} deu Impar')
+    if cond == 'P':
+        print('Você Ganhou')
+        qtd += 1
+    else:
+        print('Você perdeu')
+        break
+print(f'Game Over! Você venceu {qtd} vezes')
