e16e30cf7f92ce415e47ec38d0ec0dc0caf5ad28
Albert__igor <99365890+igoreis02@users.noreply.github.com>
2024-02-08 19:08:08 -0300

    Update ex090.py

--- ex090.py
+++ ex090.py
@@ -1,13 +1,24 @@
 alunos = dict()
+cadastro = list()
 
-alunos['nome'] = str(input('Nome do aluno: '))
-alunos['media'] = float(input('Media do aluno: '))
+while True:
+    alunos['nome'] = str(input('Nome do aluno: '))
+    alunos['media'] = float(input(f'Media de {alunos["nome"]}: '))
 
-for k, v in alunos.items():
-    print(f'{k}: {v}')
-
-if alunos['media'] >= 7:
-    print('Sutuação: Aprovado')
-else:
-    print('Situação: Reprovado')
-
+    if alunos['media'] >= 7:
+        alunos['situacao'] = 'Aprovado'
+    elif 5 <= alunos['media'] < 7:
+        alunos['situacao'] = 'Recuperação'
+    else:
+        alunos['situacao'] = 'Reprovado'
+    cadastro.append(alunos.copy())
+    opc = str(input('Deseja adicionar outro aluno? [S/N]')).upper()
+    while opc not in 'SN':
+        opc = str(input('Opção inválida > Deseja adicionar outro aluno? [S/N]')).upper()
+    if opc == 'N':
+        break
+print('-'*30)
+for c in cadastro:
+    for k, v in c.items():
+        print(f'{k}: {v}')
+    print('-'*30)
