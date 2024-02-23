37b652376700b8ae0fc4a19089568e26abff9a0f
Albert__igor <99365890+igoreis02@users.noreply.github.com>
2024-02-22 22:43:46 -0300

    Create ex108.py

---
+++ ex108.py
@@ -0,0 +1,6 @@
+import ModuloEx107and108
+p = float(input('Digite o preço: R$'))
+print(f'A metade do {ModuloEx107and108.moeda(p)} é {ModuloEx107and108.moeda(ModuloEx107and108.metade(p))}')
+print(f'P dobro do {ModuloEx107and108.moeda(p)} é {ModuloEx107and108.moeda(ModuloEx107and108.dobro(p))}')
+print(f'Aumentando 10% temos: {ModuloEx107and108.moeda(ModuloEx107and108.aumentar(p,10))}')
+print(f'Reduzindo 13% temos: {ModuloEx107and108.moeda(ModuloEx107and108.diminuir(p,13))}')
